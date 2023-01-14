c354c7654d4474aebbed04062fe2d9989b0c83c9
Serhii Chervonyi <serhii.chervonyi@orbussoftware.com>
2023-01-14 10:18:25 +0200

    Update routing

--- PolicyManager/PolicyManager/ClientApp/src/app/modules/policy/policy-routing.module.ts
+++ PolicyManager/PolicyManager/ClientApp/src/app/modules/policy/policy-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PolicyListComponent } from './components/policy-list/policy-list.component';
 import { PolicyComponent } from './components/policy/policy.component';
 
 const routes: Routes = [
-  { path: 'policy', component: PolicyComponent },
+  {
+    path: 'policy',
+    component: PolicyComponent,
+      children: [
+        {
+            path: 'list',
+            component: PolicyListComponent
+        }
+      ]
+  },
 ];
 
 @NgModule({
