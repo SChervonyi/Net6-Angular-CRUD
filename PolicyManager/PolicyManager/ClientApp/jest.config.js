bcf7a0f07fce392d2fa790d789c202df94513d8e
Serhii Chervonyi <serhii.chervonyi@orbussoftware.com>
2023-01-16 17:27:04 +0200

    Fix jest run

--- PolicyManager/PolicyManager/ClientApp/jest.config.js
+++ PolicyManager/PolicyManager/ClientApp/jest.config.js
@@ -1,5 +1,13 @@
+globalThis.ngJest = {
+  skipNgcc: true,
+  tsconfig: 'tsconfig.spec.json',
+};
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
 };
